6a0d422d6ec16dc21c11a01941d2a2fe243f6ba2
codeamac12 <codeamac12@gmail.com>
2017-01-09 17:55:09 -0600

    prueba1

---
+++ 11.rb
@@ -0,0 +1,17 @@
+title = 'title'
+description  = 'descriptin'
+number_of_likes = 123
+CONSTANT = 10
+# = > 10
+
+num_street = "Sonora"
+
+number = 15
+state = "CDMX"
+puts state
+city = "CDMX"
+num_zip = 5555
+
+
+
+
---
+++ constante.rb
@@ -0,0 +1,93 @@
+title = 'title'
+description  = 'descriptin'
+number_of_likes = 123
+CONSTANT = 10
+
+
+num_street = "Sonora"
+puts num_street
+number = 15
+puts number
+state = "CDMX"
+puts state
+city = "CDMX"
+puts city
+num_zip = 5555
+puts num_zip
+
+
+# Operaciones Aritmaticas 
+num1 = 50 
+num2 = 30 
+
+sum = num1 + num2
+puts "resultado de suma es":sum
+difference = num1 - num2
+puts "resultado de la resta es ":difference
+product = num1*num2.to_f
+puts "resultado de producto":product
+modulo = num1%num2
+puts "resultado de modulo es ":modulo
+quotient = num1/num2.to_f
+puts "resultado de division es":quotient
+
+# verdadero y falso 
+igual_que = 50==50
+igual_que = true
+
+menor_que = 23 < 30
+menor_que = true
+
+mayor_que = 54 > 40
+mayor_que = false
+
+menor_o_igual_que = 45 <= 45
+menor_o_igual_que = true
+
+mayor_o_igual_que = 34 !=34
+mayor_o_igual_que = true
+
+diferente_que = 34 !=34
+diferente_que = false 
+
+#operadores logicos 
+edad = 75
+sexo = "hombre"
+ if edad >= 70 && sexo == "hombre"
+  puts "hombre mayor"
+end
+valor_1 = (1 < 7 || false) && (true || true)
+valor_1 = true
+
+valor_2 = !false && (!false || 50 != 5**10)
+valor_2 = true
+
+valor_3 = false || !(true || true)
+valor_3 = false
+
+=begin
+para asignar
+varias 
+lineas 
+de codigo 
+=end
+
+puts '.. you can say that again..'
+puts '... can say that again..'
+
+print '... you can say that again..'
+print '.. you can say that again..'
+
+name = "roberto"
+puts "hola #{name} te escribo esta carta para invitarte a un evento esta noche"
+
+first_namee = "Roberto" 
+last_name = "Monroe" 
+gender = "M"
+puts "Welcome #{first_namee}, your last_name in #{last_name} and your gender is #{gender}"
+
+
+
+
+
+
